42e95bd2e544345ccea288879aaa64de19b04621
jacqueline-he <jh70@princeton.edu>
2021-10-08 21:39:55 -0400

    update run shell script

--- QA/run.sh
+++ QA/run.sh
@@ -2,11 +2,7 @@
 
 NUM_GPU=2
 
-PORT_ID=$(expr $RANDOM + 1000)
-
-export OMP_NUM_THREADS=4
-
-python -m torch.distributed.launch --nproc_per_node=$NUM_GPU  --master_port=$PORT_ID run_qa.py \
+python -m torch.distributed.launch --nproc_per_node=$NUM_GPU run_qa.py \
     --model_name_or_path bert-base-uncased \
     --dataset_name squad \
     --do_train \
@@ -16,5 +12,5 @@ python -m torch.distributed.launch --nproc_per_node=$NUM_GPU  --master_port=$POR
     --max_seq_length 384 \
     --doc_stride 128 \
     --output_dir ./results \
-    --per_device_eval_batch_size=1   \
-    --per_device_train_batch_size=1   
+    --per_device_eval_batch_size=4   \
+    --per_device_train_batch_size=4   
